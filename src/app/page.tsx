2076a12c7190ae515099f608560f3c0a85ca1ee5
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-12 14:51:47 +0900

    feat: profile component 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import PostList from "@/components/PostList";
+import Profile from "@/components/Profile";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Profile />
       <PostList />
     </main>
   );
---
+++ src/components/Profile.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Profile() {
+  return (
+    <div className="w-auto h-auto p-2 flex justify-center items-center gap-5">
+      <div>
+        <h1 className="text-lg text-gray-800 font-semibold">Hello</h1>
+        <span className="text-md text-gray-600">I am heywon0909</span>
+      </div>
+      <Image
+        src={"/blog/images/profile.png"}
+        alt="profile"
+        width="180"
+        height="180"
+      />
+    </div>
+  );
+}
