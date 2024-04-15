f488ff228de3e0bf1433673933d3edb358c3dde5
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-15 10:33:20 +0900

    feat: contact page create

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,17 @@
+"use client";
+import { useRouter } from "next/navigation";
 import HeaderList from "./HeaderList";
 
 export default function Header() {
+  const router = useRouter();
+  const handleMain = () => router.push("/", { scroll: false });
+
   return (
-    <header className="w-full h-20 flex items-center p-2 justify-center bg-white">
+    <header className="w-full h-20 flex items-center p-2 justify-center bg-white cursor-pointer">
       <div className="w-5/6 flex justify-around">
-        <h1 className="font-semibold text-2xl">heywon0909</h1>
+        <h1 className="font-semibold text-2xl" onClick={handleMain}>
+          heywon0909
+        </h1>
         <HeaderList />
       </div>
     </header>
--- src/components/header/HeaderList.tsx
+++ src/components/header/HeaderList.tsx
@@ -4,9 +4,9 @@ export default function HeaderList() {
   return (
     <div className="flex gap-4 text-base font-mono">
       <Link href="/">Home</Link>
-      <Link href="/">Home</Link>
-      <Link href="/">Home</Link>
-      <Link href="/">Home</Link>
+      <Link href="/about">about</Link>
+      <Link href="/posts">posts</Link>
+      <Link href="/contact">contact</Link>
     </div>
   );
 }
