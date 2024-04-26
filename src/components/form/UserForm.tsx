c6a6a74e906a585b40133301a13c8a555b15d292
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-26 17:55:22 +0900

    style: contact form style

--- src/components/form/UserForm.tsx
+++ src/components/form/UserForm.tsx
@@ -26,6 +26,7 @@ interface Props {
 export default function UserForm({ handleSubmit }: Props) {
   const [state, formAction] = useFormState(handleSubmit, null);
   const { pending } = useFormStatus();
+
   return (
     <>
       <form
@@ -46,12 +47,15 @@ export default function UserForm({ handleSubmit }: Props) {
                 name="email"
                 id="email"
                 autoComplete="email"
-                className=" w-full block  border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                className="p-2 w-full block  border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                 placeholder="email을 입력해 주세요."
               />
             </div>
           </div>
         </div>
+        {state?.error?.email && (
+          <p className=" text-red-400">{state?.error.email} 😭</p>
+        )}
         <div className="sm:col-span-4">
           <label
             htmlFor="subject"
@@ -66,30 +70,34 @@ export default function UserForm({ handleSubmit }: Props) {
                 name="subject"
                 id="subject"
                 autoComplete="subject"
-                className=" w-full block  border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                className="p-2 w-full block  border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                 placeholder="subject를 입력해 주세요."
               />
             </div>
           </div>
         </div>
+        {state?.error?.subject && (
+          <p className=" text-red-400">{state?.error.subject} 😭</p>
+        )}
         <label htmlFor="message">message</label>
         <textarea
           id="message"
           name="message"
-          className="resize-none h-56 text-black flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600"
+          className="resize-none h-56 text-black flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 p-2"
         />
+        {state?.error?.message && (
+          <p className=" text-red-400">{state?.error.message} 😭</p>
+        )}
         <button
           type="submit"
           disabled={pending}
-          className={`w-full bg-slate-300 text-black h-10 rounded-md ${
-            pending ? "bg-slate-100" : "bg-slate-300"
+          className={`w-full bg-slate-300 h-10 rounded-md hover:bg-slate-500 text-gray-300 ${
+            pending ? "bg-slate-100" : "bg-slate-700"
           }`}
         >
           submit
         </button>
       </form>
-      {state?.error && <p className="text-white">{state?.error.email} 😭</p>}
-      {state?.message && <p className="text-white">{state?.message}</p>}
     </>
   );
 }
