0c410640ce6a8ce08392e1cae07bf735f50b87ca
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-26 17:48:36 +0900

    feat: contact route api delete

--- src/app/api/contact/route.ts
+++
@@ -1,7 +0,0 @@
-import { handleSubmit } from "@/service/form";
-
-export async function POST(data: FormData) {
-  const result = await handleSubmit(data);
-  console.log("여기타니", result);
-  return result.message;
-}
