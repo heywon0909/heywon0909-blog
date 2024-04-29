f7fa8305da52f841ca6f774bede3694b3be60544
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-29 17:28:38 +0900

    style: header style css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,7 +9,10 @@ export default function Header() {
   return (
     <header className="absolute top-0 w-full h-20 flex items-center p-2 justify-center cursor-pointer">
       <div className="w-5/6 flex justify-around">
-        <h1 className="font-semibold text-2xl text-white" onClick={handleMain}>
+        <h1
+          className="font-semibold text-2xl text-zinc-200 hover:text-white"
+          onClick={handleMain}
+        >
           heywon0909
         </h1>
         <HeaderList />
--- src/components/header/HeaderList.tsx
+++ src/components/header/HeaderList.tsx
@@ -2,11 +2,19 @@ import Link from "next/link";
 
 export default function HeaderList() {
   return (
-    <div className="flex gap-4 text-base text-white">
-      <Link href="/">Home</Link>
-      <Link href="/about">about</Link>
-      <Link href="/posts">posts</Link>
-      <Link href="/contact">contact</Link>
+    <div className="flex gap-4 text-base text-zinc-200 ">
+      <Link href="/" className="hover:text-white">
+        Home
+      </Link>
+      <Link href="/about" className="hover:text-white">
+        about
+      </Link>
+      <Link href="/posts" className="hover:text-white">
+        posts
+      </Link>
+      <Link href="/contact" className="hover:text-white">
+        contact
+      </Link>
     </div>
   );
 }
