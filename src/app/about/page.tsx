e693fe34d9e81c560d15cc7a20628a882fccffca
heywon0909 <hyewon_park2@tmax.co.kr>
2024-04-30 10:37:38 +0900

    feat: next/image 기능 설정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,9 +2,11 @@ import Image from "next/image";
 export default function page() {
   return (
     <div className=" w-2/3 flex flex-col gap-4 justify-center items-center ">
-      <div className="w-48 h-48 rounded-full ">
+      <div className="w-48 h-48 rounded-full bg-red-50">
         <Image
-          src={"/blog/images/profile.png"}
+          src={
+            "https://github.com/heywon0909/heywon0909/assets/50330312/97bb6be8-8215-40e6-ad4f-01cd327fc522"
+          }
           alt="profile"
           width="180"
           height="180"
@@ -13,8 +15,8 @@ export default function page() {
       <h2 className="text-xl text-white">
         안녕하세요 <span className="font-semibold">박혜원</span>입니다.
       </h2>
-      <div className="w-auto p-4 bg-purple-50 rounded-md font-normal">
-        <ul className="flex flex-col gap-3  text-slate-900">
+      <div className="w-auto p-4  bg-gray-800 rounded-md font-normal">
+        <ul className="flex flex-col gap-3  text-white">
           <ol>1. React, Vue js 활용한 실무 적용 경험이 있습니다.</ol>
           <li className="pl-6">- Vue js 실무 경험 (2년)</li>
           <li className="pl-6">- React 팀프로젝트 + 실무 (1년)</li>
